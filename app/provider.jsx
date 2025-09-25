34ca8cda2490322e44f39ea4cd1017429586da5d
22emini <emininioluwaakinrinade716@example.com>
2025-09-25 22:50:47 +0100

    sidebar completed

--- app/provider.jsx
+++ app/provider.jsx
@@ -3,7 +3,7 @@ import { UserDetailContext } from '@/context/UserDetailContext';
 import { useUser } from '@clerk/nextjs'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-
+// this function is used store new user inside database
 function Provider({ children }) {
   const { user } = useUser();
   const [ userDetail , setUserDetail ] = useState();
---
+++ app/workspace/_components/AppSideBar.jsx
@@ -0,0 +1,81 @@
+'use client'
+import React, { use } from 'react'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { Book, Compass, icons, LayoutDashboard, PencilRulerIcon, UserCircle2Icon } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+function AppSideBar() {
+    const  SideBarOptions=[{
+        title:"Dashboard",
+        icon:LayoutDashboard,
+        path:'/workspace'
+    },
+    {
+        title:"My Learning",
+        icon:Book,
+        path:'/workspace/my-courses'
+    },
+    {
+        title:"Explore Courses",
+        icon:Compass,
+        path:'/workspace/explore'
+    },
+    {
+        title:"AI Tools",
+        icon:PencilRulerIcon,
+        path:'/workspace/ai-tools'
+    },
+    {
+        title:"Profile",
+        icon:UserCircle2Icon,
+        path:'/workspace/profile'
+    },
+]
+const path = usePathname();
+  return (
+   
+    <Sidebar className={'pt-'}>
+      <SidebarHeader >
+        <Image src={'/logo.svg'}  alt='logo' width={120} height={100} />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup >
+            <Button> Create New Course</Button>
+        </SidebarGroup>
+        <SidebarGroup >
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {SideBarOptions.map((item, index)=>(
+                        <SidebarMenuItem key={index} >
+                            <SidebarMenuButton asChild className={'p-5'}>
+                                <Link href={item.path} className={`text-[17px] ${path.includes(item.path)&&'text-primary bg-purple-100'}`}> 
+                                <item.icon className='h-7 w-7'/>
+                                <span  className=''>{item.title}</span>
+                                </Link>
+                             
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                     </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+
+  )
+}
+
+export default AppSideBar
